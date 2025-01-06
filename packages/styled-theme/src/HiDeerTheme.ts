b82e8918e76e444fe1cd766db63cab7f7faa18a7
ftery0 <lerb5253@dgsw.hs.kr>
2025-01-06 16:02:17 +0900

    feat :: theme

---
+++ packages/styled-theme/src/HiDeerTheme.ts
@@ -0,0 +1,81 @@
+import { DefaultTheme } from "styled-components";
+
+
+export const lightTheme: DefaultTheme = {
+  //primary
+  PrimaryNormal: "#FFA800",
+  PrimaryAlternative: "rgba(255, 168, 0, 0.65)",
+
+  //Label
+  LabelNormal: "#0F0F10",
+  LabelStrong: "#000000",
+  LabelNeutral: "#383A3B",
+  LabelAlternative: "#5D5F60",
+  LabelAssistive: "#747678",
+  LabelDisable: "#E6E6E7",
+
+  //Line
+  LineNormal: "#C4C5C6",
+  LineNeutral: "#DCDDDE",
+  LineAlternative: "#E6E6E7",
+
+  //Fill
+  FillNormal: "#F5F5F5",
+  FillNeutral: "#E6E6E7",
+  FillAlternative: "#DCDDDE",
+
+  //Background
+  BackgroundNormal: "#FFFFFF",
+  BackgroundNeutral: "#F5F5F5",
+  BackgroundAlternative: "#EAEAEA",
+
+  //Status
+  StatusNegative: "#FF4242",
+  StatusCautionary: "#FFC800",
+  StatusPositive: "#00BF40",
+
+  //Static
+  StaticWhite: "#FFFFFF",
+  StaticBlack: "#000000"
+
+};
+
+export const darkTheme: DefaultTheme = {
+
+  //primary
+  PrimaryNormal: "#FFA800",
+  PrimaryAlternative: "rgba(255, 168, 0, 0.65)",
+
+  //Label
+  LabelNormal: "#0F0F10",
+  LabelStrong: "#FFFFFF",
+  LabelNeutral: "#DCDDDE",
+  LabelAlternative: "#C4C5C6",
+  LabelAssistive: "#9B9D9F",
+  LabelDisable: "#48494A",
+
+  //Line
+  LineNormal: "#747678",
+  LineNeutral: "#48494A",
+  LineAlternative: "#383A3B",
+
+  //Fill
+  FillNormal: "#2A2B2C",
+  FillNeutral: "#383A3B",
+  FillAlternative: "#48494A",
+
+  //Background
+  BackgroundNormal: "#232424",
+  BackgroundNeutral: "#191A1A",
+  BackgroundAlternative: "#111212",
+
+  //Status
+  StatusNegative: "#FF4242",
+  StatusCautionary: "#FFC800",
+  StatusPositive: "#00BF40",
+
+  //Static
+  StaticWhite: "#FFFFFF",
+  StaticBlack: "#000000"
+
+};
