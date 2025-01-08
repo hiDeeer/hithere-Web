e29efb0c99866a4b546dc67713a47267461534ea
ftery0 <lerb5253@dgsw.hs.kr>
2025-01-08 11:01:45 +0900

    fix :: icon 대문자 변경

--- packages/icon/index.ts
+++ packages/icon/index.ts
@@ -3,13 +3,15 @@ export * from "./src/Icon/Chat";
 export * from "./src/Icon/Favorite";
 export * from "./src/Icon/Home";
 export * from "./src/Icon/Trash";
-export * from "./src/Icon/check_ring_round";
-export * from "./src/Icon/closeEyes";
-export * from "./src/Icon/close_ring";
-export * from "./src/Icon/openEye";
-export * from "./src/Icon/sound_max";
-export * from "./src/Icon/world";
+export * from "./src/Icon/Check_ring_rounds";
+export * from "./src/Icon/CloseEye";
+export * from "./src/Icon/Close_rings";
+export * from "./src/Icon/OpenEyes";
+export * from "./src/Icon/Sound_maxs";
+export * from "./src/Icon/Worlds";
 export * from "./src/Icon/arrowDown";
 
 
-// export * from "./src/Logo";
+export * from "./src/Logo/LoginLogo";
+export * from "./src/Logo/SignUpLogo1";
+export * from "./src/Logo/SignUpLogo2";
