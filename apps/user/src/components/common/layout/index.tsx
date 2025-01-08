fb56d319d1b90e2498135f77afe37a5a3ff39a82
ftery0 <lerb5253@dgsw.hs.kr>
2025-01-08 10:37:23 +0900

    init :: user

---
+++ apps/user/src/components/common/layout/index.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Container, Wrap } from "./style";
+import { usePathname } from "next/navigation";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  const pathname = usePathname();
+  return (
+    <>
+      <Container>
+        {pathname !== "/sign"}
+        <Wrap>
+          {children}
+          <div id="modal"></div>
+        </Wrap>
+      </Container>
+    </>
+  );
+};
+
+export default Layout;
---
+++ apps/user/src/components/common/layout/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  background-color: #f5f5f5;
+
+  overflow-y: hidden;
+`;
+
+export const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
